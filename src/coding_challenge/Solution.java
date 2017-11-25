bf447ac9088a7a248ab1239be5cf00a43cde1e1a
WON SEOB SEO <pghoou@hotmail.com>
2017-11-25 15:55:34 +0200

    Java Int type?

---
+++ src/coding_challenge/Solution.java
@@ -0,0 +1,15 @@
+package coding_challenge;
+
+/**
+ * Created by wseobseo on 25/11/2017.
+ */
+public class Solution {
+
+    public static void main(String [] args)
+    {
+        int[] thisIsAnIntArray = new int[] {1, 3, 5};
+        System.out.println(thisIsAnIntArray.getClass());
+        System.out.println(thisIsAnIntArray);
+    }
+
+}
