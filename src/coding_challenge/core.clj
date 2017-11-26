eb98447f546781f22f3e89fec394f4703e95ce3f
WON SEOB SEO <pghoou@hotmail.com>
2017-11-26 09:58:43 +0200

    Pass example test cases

--- src/coding_challenge/core.clj
+++ src/coding_challenge/core.clj
@@ -4,5 +4,9 @@
 
 ; (set! *unchecked-math* true) ;for the performance reason
 
-(defn ^int solution [^ints x] ;type hints for performance
-  )
+(defn ^int solution [^ints xs] ;type hints for performance
+  (let [int-seq (int-array (take (inc (alength xs)) (rest (range))))]
+    (try (areduce xs i _ nil
+                  (aset int-seq (dec (aget xs i)) 0))
+         (catch Throwable _))
+    (first (filter #(not (zero? %)) int-seq))))
