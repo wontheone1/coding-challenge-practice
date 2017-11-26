25e3942585503c666d4e140a556d24d3bd73d801
WON SEOB SEO <pghoou@hotmail.com>
2017-11-26 13:32:41 +0200

    Add persistent vector solution

--- src/coding_challenge/core.clj
+++ src/coding_challenge/core.clj
@@ -4,17 +4,27 @@
 
 ; (set! *unchecked-math* true) ;for the performance reason
 
-(defn solution [^ints xs] ;type hints for performance
-  (let [int-seq (int-array (take (inc (alength xs)) (rest (range))))]
+(defn solution-by-int-array-sequential-impl [^ints xs]      ;type hints for performance
+  (let [int-seq (int-array (take (+ 2 (alength xs)) (range)))]
     (areduce xs i _ nil
-             (try (aset int-seq (dec (aget xs i)) 0)
+             (try (aset int-seq (aget xs i) 0)
                   (catch Throwable _)))
     (first (filter #(not (zero? %)) int-seq))))
 
-(defn solution2 [^ints xs]                                  ;type hints for performance
-  (let [int-seq (int-array (take (inc (alength xs)) (rest (range))))]
+(defn solution-by-persistent-vector [^ints xs]              ;type hints for performance
+  (let [int-vec (vec (take (+ 2 (alength xs)) (range)))
+        not-occurring-ints (reduce (fn [int-vec num]
+                                     (try (assoc int-vec num 0)
+                                          (catch Throwable _
+                                            int-vec)))
+                                   int-vec
+                                   xs)]
+    (first (filter #(not (zero? %)) not-occurring-ints))))
+
+(defn solution-by-int-array-parallel-impl [^ints xs]        ;type hints for performance
+  (let [int-seq (int-array (take (+ 2 (alength xs)) (range)))]
     (doall (pmap (fn [num]
-                   (try (aset int-seq (dec num) 0)
+                   (try (aset int-seq num 0)
                         (catch Throwable _)))
                  xs))
     (first (filter #(not (zero? %)) int-seq))))
--- test/coding_challenge/core_test.clj
+++ test/coding_challenge/core_test.clj
@@ -10,21 +10,33 @@
   (fact
     "test 1"
     (let [input (int-array [1 3 6 4 1 2])]
-      (= 5 (solution input) (solution2 input))) => true)
+      (= 5
+         (solution-by-int-array-sequential-impl input)
+         (solution-by-int-array-parallel-impl input)
+         (solution-by-persistent-vector input))) => true)
   (fact
     "test 2"
     (let [input (int-array [1 2 3])]
-      (= 4 (solution input) (solution2 input))) => true)
+      (= 4
+         (solution-by-int-array-sequential-impl input)
+         (solution-by-int-array-parallel-impl input)
+         (solution-by-persistent-vector input))) => true)
   (fact
     "test 3"
     (let [input (int-array [-1 -3])]
-      (= 1 (solution input) (solution2 input))) => true)
+      (= 1
+         (solution-by-int-array-sequential-impl input)
+         (solution-by-int-array-parallel-impl input)
+         (solution-by-persistent-vector input))) => true)
   (fact
     "test 4"
     (let [input (int-array [1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 3 3 3 3 3 3 3 4 4
                             6 7 7 8 8 9 9 10 11 11 12 13 13 14 15 15 15 16 18 24 26
                             27 28 30 31 34 36 36 36 45 46 47 49 53 55 61 67 68 80 89 93 108 126])]
-      (= 5 (solution input) (solution2 input))) => true)
+      (= 5
+         (solution-by-int-array-sequential-impl input)
+         (solution-by-int-array-parallel-impl input)
+         (solution-by-persistent-vector input))) => true)
   (fact
     "test 5"
     (let [input (int-array [4 -96 -4 -2 0 -1 0
@@ -42,7 +54,10 @@
                             -3 0 1 68 20 -1 -60 1 -1 103 -13
                             20 58 -4 -150
                             1 0])]
-      (= 8 (solution input) (solution2 input))) => true))
+      (= 8
+         (solution-by-int-array-sequential-impl input)
+         (solution-by-int-array-parallel-impl input)
+         (solution-by-persistent-vector input))) => true))
 
 (def element-gen
   (gen/large-integer* {:min -1000000, :max 1000000}))
@@ -51,18 +66,24 @@
   (prop/for-all [input-ints (gen/fmap #(int-array %)
                                       (gen/not-empty (gen/vector element-gen 100000)))]
                 (do
-                  #_(println :answer (solution ^ints input-ints))
+                  #_(println :answer (solution-by-int-array-sequential-impl ^ints input-ints))
                   #_(println (take 200 (filter pos? (sort ^ints input-ints))))
                   (> (solution-fn ^ints input-ints) 0))))
 
 (time
   (fact
-    (println :solution-time)
-    (tc/quick-check 3 (find-by-solution solution))
+    (println :solution-by-int-array-sequential-impl)
+    (tc/quick-check 1 (find-by-solution solution-by-int-array-sequential-impl))
     => #(:result %)))
 
 (time
   (fact
-    (println :solution2-time)
-    (tc/quick-check 3 (find-by-solution solution2))
+    (println :solution-by-persistent-vector)
+    (tc/quick-check 1 (find-by-solution solution-by-persistent-vector))
+    => #(:result %)))
+
+(time
+  (fact
+    (println :solution-by-int-array-parallel-impl)
+    (tc/quick-check 1 (find-by-solution solution-by-int-array-parallel-impl))
     => #(:result %)))
