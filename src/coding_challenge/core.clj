8d20041d33636ba5ede22ad487e72aae01b3962d
WON SEOB SEO <pghoou@hotmail.com>
2017-11-26 12:37:44 +0200

    Add parallel solution

--- src/coding_challenge/core.clj
+++ src/coding_challenge/core.clj
@@ -10,3 +10,11 @@
              (try (aset int-seq (dec (aget xs i)) 0)
                   (catch Throwable _)))
     (first (filter #(not (zero? %)) int-seq))))
+
+(defn solution2 [^ints xs]                                  ;type hints for performance
+  (let [int-seq (int-array (take (inc (alength xs)) (rest (range))))]
+    (doall (pmap (fn [num]
+                   (try (aset int-seq (dec num) 0)
+                        (catch Throwable _)))
+                 xs))
+    (first (filter #(not (zero? %)) int-seq))))
--- test/coding_challenge/core_test.clj
+++ test/coding_challenge/core_test.clj
@@ -9,48 +9,60 @@
   "Some tests"
   (fact
     "test 1"
-    (solution (int-array [1 3 6 4 1 2])) => 5)
+    (let [input (int-array [1 3 6 4 1 2])]
+      (= 5 (solution input) (solution2 input))) => true)
   (fact
     "test 2"
-    (solution (int-array [1 2 3])) => 4)
+    (let [input (int-array [1 2 3])]
+      (= 4 (solution input) (solution2 input))) => true)
   (fact
     "test 3"
-    (solution (int-array [-1 -3])) => 1)
+    (let [input (int-array [-1 -3])]
+      (= 1 (solution input) (solution2 input))) => true)
   (fact
     "test 4"
-    (solution (int-array [-1 -3])) => 1)
+    (let [input (int-array [1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 3 3 3 3 3 3 3 4 4
+                            6 7 7 8 8 9 9 10 11 11 12 13 13 14 15 15 15 16 18 24 26
+                            27 28 30 31 34 36 36 36 45 46 47 49 53 55 61 67 68 80 89 93 108 126])]
+      (= 5 (solution input) (solution2 input))) => true)
   (fact
     "test 5"
-    (solution (int-array
-                [4 -96 -4 -2 0 -1 0
-                 7 1
-                 5
-                 0 -7 -6 21 0 -95 -4 0 0 -1 -13 -12 1
-                 73 -1 -90 0 14 -27 -1 -2
-                 -23 -232 0 18 0 -2 -1 13 -40 -7 3
-                 -1 4 4
-                 -2 2
-                 -14 -76 -24 -5 31 -10 -9 -11
-                 -1 -13 -4 0 5 2 -4 -245 -2 55
-                 47 -2 26 7
-                 5 -1 119 -5 -1
-                 4 -1 -1 6
-                 -3 0 1 68 20 -1 -60 1 -1 103 -13
-                 20 58 -4 -150
-                 1 0])) => 8))
+    (let [input (int-array [4 -96 -4 -2 0 -1 0
+                            7 1
+                            5
+                            0 -7 -6 21 0 -95 -4 0 0 -1 -13 -12 1
+                            -23 -232 0 18 0 -2 -1 13 -40 -7 3
+                            -1 4 4
+                            -2 2
+                            -14 -76 -24 -5 31 -10 -9 -11
+                            -1 -13 -4 0 5 2 -4 -245 -2 55
+                            47 -2 26 7
+                            5 -1 119 -5 -1
+                            4 -1 -1 6
+                            -3 0 1 68 20 -1 -60 1 -1 103 -13
+                            20 58 -4 -150
+                            1 0])]
+      (= 8 (solution input) (solution2 input))) => true))
 
 (def element-gen
   (gen/large-integer* {:min -1000000, :max 1000000}))
 
-(def find-solution
+(defn find-by-solution [solution-fn]
   (prop/for-all [input-ints (gen/fmap #(int-array %)
                                       (gen/not-empty (gen/vector element-gen 100000)))]
                 (do
                   #_(println :answer (solution ^ints input-ints))
                   #_(println (take 200 (filter pos? (sort ^ints input-ints))))
-                  (> (solution ^ints input-ints) 0))))
+                  (> (solution-fn ^ints input-ints) 0))))
 
 (time
   (fact
-    (tc/quick-check 10 find-solution)
+    (println :solution-time)
+    (tc/quick-check 3 (find-by-solution solution))
+    => #(:result %)))
+
+(time
+  (fact
+    (println :solution2-time)
+    (tc/quick-check 3 (find-by-solution solution2))
     => #(:result %)))
