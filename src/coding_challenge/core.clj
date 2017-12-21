87346eacb62e7ab75720978523f5751a0e869010
WON SEOB SEO <pghoou@hotmail.com>
2017-12-21 13:09:27 +0200

    Improve performance of the solution

--- src/coding_challenge/core.clj
+++ src/coding_challenge/core.clj
@@ -2,40 +2,29 @@
 
 (set! *warn-on-reflection* true)
 
-(defn- count-possible-move-pattern [move-pattern-length hunted-nests hunting-nests opposite-nests cnt]
+(defn- count-possible-move-pattern [hunting-nests opposite-nests cnt]
   (cond
-    (= move-pattern-length (count hunted-nests))
-    1
-
     (empty? hunting-nests)
-    0
+    cnt
 
     :else
-    (apply + (for [[_ hunted-nest-height :as hunted-nest] hunting-nests]
-               (count-possible-move-pattern move-pattern-length
-                                            (cons hunted-nest hunted-nests)
-                                            (remove #(< (second %) hunted-nest-height) opposite-nests)
-                                            (remove #{hunted-nest} hunting-nests)
-                                            cnt)))))
+    (apply + (for [[_ hunted-nest-height] hunting-nests]
+               (count-possible-move-pattern (remove #(< (second %) hunted-nest-height) opposite-nests)
+                                            hunting-nests
+                                            (inc cnt))))))
 
 (defn solution [^ints input-ints]
   (let [nests (map #(do [%1 %2]) (range) (vec input-ints))]
-    (apply + (count nests)
-           (for [move-pattern-length (range 2 (inc (count nests)))]
-             (apply +
-                    (for [[nest-index nest-height :as nest] nests
-                          :let [left-nests (filter #(< (first %) nest-index) nests)
-                                right-nests (filter #(> (first %) nest-index) nests)
-                                left-nests-at-higher (filter #(> (second %) nest-height) left-nests)
-                                right-nests-at-higher (filter #(> (second %) nest-height) right-nests)]]
-                      (+ (count-possible-move-pattern move-pattern-length
-                                                      '(nest)
-                                                      left-nests-at-higher
-                                                      right-nests-at-higher
-                                                      0)
+    (apply + (alength input-ints)
+           (for [[nest-index nest-height :as nest] nests
+                 :let [left-nests (filter #(< (first %) nest-index) nests)
+                       right-nests (filter #(> (first %) nest-index) nests)
+                       left-nests-at-higher (filter #(> (second %) nest-height) left-nests)
+                       right-nests-at-higher (filter #(> (second %) nest-height) right-nests)]]
+             (+ (count-possible-move-pattern left-nests-at-higher
+                                             right-nests-at-higher
+                                             0)
 
-                         (count-possible-move-pattern move-pattern-length
-                                                      '(nest)
-                                                      right-nests-at-higher
-                                                      left-nests-at-higher
-                                                      0))))))))
+                (count-possible-move-pattern right-nests-at-higher
+                                             left-nests-at-higher
+                                             0))))))
--- test/coding_challenge/core_test.clj
+++ test/coding_challenge/core_test.clj
@@ -14,11 +14,12 @@
     (sol/solution input)) => 7)
 
 (def int-gen
-  (gen/large-integer* {:min -1000000, :max 1000000}))
+  (gen/large-integer* {:min 1, :max 1000000000}))
 
 (defn find-by-solution [solution-fn number-of-ints]
   (prop/for-all [input-ints (gen/fmap #(int-array %)
-                                      (gen/not-empty (gen/vector int-gen number-of-ints)))]
+                                      (gen/not-empty (gen/vector-distinct int-gen
+                                                                          {:num-elements number-of-ints})))]
                 (do
                   #_(println :answer (solution ^ints input-ints))
                   #_(println (seq input-ints))
@@ -28,12 +29,13 @@
   (println :time-measure-with number-of-ints "ints")
   (time
     (fact
-      (println :solution)
       (tc/quick-check 1 (find-by-solution sol/solution number-of-ints))
       true
       => true)))
 
-(measure-performance 3)
-(measure-performance 6)
-(measure-performance 12)
+(measure-performance 2)
+(measure-performance 4)
+(measure-performance 8)
 (measure-performance 16)
+(measure-performance 32)
+(measure-performance 36)
