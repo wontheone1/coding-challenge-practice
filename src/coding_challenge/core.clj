529677fc3ceeaa7e100f57e653c3e43e21536a06
WON SEOB SEO <won.seob.seo@zalando.fi>
2018-09-08 00:05:34 +0300

    Use solution proportional to total bribery

--- src/coding_challenge/core.clj
+++ src/coding_challenge/core.clj
@@ -1,32 +1,26 @@
 (ns coding-challenge.core)
 
-(defn- increase-bribe-count [{:keys [bribed-person-num->bribe-count] :as result} person-num]
-  (let [bribed-persons (keys bribed-person-num->bribe-count)]
-    (reduce (fn [result bribed-person]
-              (if (> bribed-person person-num)
-                (if (= 2 (get-in result [:bribed-person-num->bribe-count bribed-person]))
-                  (assoc result :possible false)
-                  (update-in result [:bribed-person-num->bribe-count bribed-person] inc))
-                result))
-            result
-            bribed-persons)))
-
 (defn minimumBribes [num-person q]
-  (let [result (loop [index 1
-                      result {:bribed-person-num->bribe-count {}
-                              :possible                       true}]
-                 (if (and (<= index num-person) (:possible result))
-                   (let [person-num (q (dec index))]
-                     (recur (inc index)
-                            (increase-bribe-count
-                              (assoc-in result [:bribed-person-num->bribe-count person-num] 0)
-                              person-num)))
-                   result))]
-    (if (:possible result)
-      (println (apply + (vals (:bribed-person-num->bribe-count result))))
+  (let [result (loop [i 0
+                      count 0]
+                 (if (< i num-person)
+                   (if (< 3 (- (q i) i))
+                     false
+                     (let [num-qi-taken-over (loop [j (max 0 (- (q i) 2))
+                                                    num-qi-taken-over 0]
+                                               (if (< j i)
+                                                 (recur (inc j)
+                                                        (if (> (q j) (q i))
+                                                          (inc num-qi-taken-over)
+                                                          num-qi-taken-over))
+                                                 num-qi-taken-over))]
+                       (recur (inc i) (+ count num-qi-taken-over))))
+                   count))]
+    (if result
+      (println result)
       (println "Too chaotic"))))
 
-(defn -main []
+(defn solve []
   (let [num-person 5
         q (vec (map #(Integer/parseInt %) (clojure.string/split "2 1 5 3 4" #" ")))]
     (minimumBribes num-person q)))
