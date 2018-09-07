7f12d4740072d23a0830bf89d0615c6bd68fde1e
WON SEOB SEO <won.seob.seo@zalando.fi>
2018-09-07 20:32:06 +0300

    Solve with 22 points solution

--- src/coding_challenge/core.clj
+++ src/coding_challenge/core.clj
@@ -1,6 +1,32 @@
 (ns coding-challenge.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- increase-bribe-count [{:keys [bribed-person-num->bribe-count] :as result} person-num]
+  (let [bribed-persons (keys bribed-person-num->bribe-count)]
+    (reduce (fn [result bribed-person]
+              (if (> bribed-person person-num)
+                (if (= 2 (get-in result [:bribed-person-num->bribe-count bribed-person]))
+                  (assoc result :possible false)
+                  (update-in result [:bribed-person-num->bribe-count bribed-person] inc))
+                result))
+            result
+            bribed-persons)))
+
+(defn minimumBribes [num-person q]
+  (let [result (loop [index 1
+                      result {:bribed-person-num->bribe-count {}
+                              :possible                       true}]
+                 (if (and (<= index num-person) (:possible result))
+                   (let [person-num (q (dec index))]
+                     (recur (inc index)
+                            (increase-bribe-count
+                              (assoc-in result [:bribed-person-num->bribe-count person-num] 0)
+                              person-num)))
+                   result))]
+    (if (:possible result)
+      (println (apply + (vals (:bribed-person-num->bribe-count result))))
+      (println "Too chaotic"))))
+
+(defn -main []
+  (let [num-person 5
+        q (vec (map #(Integer/parseInt %) (clojure.string/split "2 1 5 3 4" #" ")))]
+    (minimumBribes num-person q)))
